c7adeff98f874cf28be80fa9a5ebffeefd7e898b
ponta <coshun@gmail.com>
2026-08-09 10:41:10 +0900

    feat(series-comparison): connect rank signals to review

--- apps/api/src/main/scala/momo/api/usecases/seriescomparison/SeriesComparisonReviewAggregation.scala
+++ apps/api/src/main/scala/momo/api/usecases/seriescomparison/SeriesComparisonReviewAggregation.scala
@@ -1,6 +1,8 @@
 package momo.api.usecases.seriescomparison
 
-import momo.api.usecases.seriescomparison.engine.SeriesDataset
+import scala.util.Try
+
+import momo.api.usecases.seriescomparison.engine.{SeriesDataset, SeriesRankAnalyzer}
 import momo.api.usecases.seriescomparison.view.*
 
 private[usecases] object SeriesComparisonReviewAggregation
@@ -12,7 +14,7 @@ private[usecases] object SeriesComparisonReviewAggregation
     with SeriesComparisonReviewPlayOrderCandidate
     with SeriesComparisonReviewRecoveryCandidate
     with SeriesComparisonReviewGinjiCandidate:
-  private val SchemaVersion = 4
+  private val SchemaVersion = 5
 
   def aggregate(
       dataset: SeriesDataset
@@ -28,12 +30,26 @@ private[usecases] object SeriesComparisonReviewAggregation
       .flatMap(memberId => playbookCandidates(statsByPlayer(memberId), orderedRows))
     val scoredCandidates = SeriesComparisonPlaybookScoring.score(allCandidates)
     val commonTopics = SeriesComparisonPlaybookScoring.commonTopics(scoredCandidates)
+    val directCardsByPlayer = playerOrder.map(memberId =>
+      memberId -> SeriesComparisonPlaybookScoring.cardsFor(memberId, scoredCandidates)
+    ).toMap
+    val canUseSecondaryRankSignal = directCardsByPlayer.values.exists(cards =>
+      cards.nonEmpty && cards.size < 3
+    )
+    val rankAnalysis = Option.when(canUseSecondaryRankSignal)(
+      Try(SeriesRankAnalyzer.analyzeForDrilldown(dataset)).toOption
+    ).flatten
     val playbook = playerOrder.map(memberId =>
       val stats = statsByPlayer(memberId)
+      val directCards = directCardsByPlayer.getOrElse(memberId, Nil)
       SeriesComparisonPlayerPlaybookView(
         memberId = memberId.value,
         memberDisplayName = stats.displayName,
-        cards = SeriesComparisonPlaybookScoring.cardsFor(memberId, scoredCandidates),
+        cards = SeriesComparisonRankSignalReviewSupport.appendSecondaryCard(
+          memberId,
+          directCards,
+          rankAnalysis,
+        ),
       )
     )
     SeriesComparisonReviewView(
